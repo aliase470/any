8067d2d57c685fe9fa1e224cdbd89e704356ec05
aliase470 <122420199+aliase470@users.noreply.github.com>
2023-06-21 08:08:19 +0530

    Add files via upload

--- Blockchain/p5.sol
+++ Blockchain/p5.sol
@@ -1,16 +1,30 @@
 /* write a solidity program to demonstrate to find if number is  Odd Or Even & Prime Or Composite*/
 pragma solidity >=0.8.2 <0.9.0;
+contract test{
+    int num;
+    string num_type;
+     function set(int n) public{
+        if(n%2==0){
+            num_type='Even';
+        }else{
+            num_type='Odd';
+        }
+     }
+     function get()public view return(string  memory){
+        return num_type;
+     }
+}
 
 pragma solidity >=0.8.2 <0.9.0;
 contract PrimeNumberChecker {
     function isPrime (uint n) public pure returns (string memory){
         for (uint i = 2;i<n;i++){
             if(n % i ==0){
-                return "Not a prime";
+                 return "Not a prime";
                }
-                else{
-                    return "prime";
+               else{
+                return "prime";
                 }
-                    }
-                    }
-                    }
+                }
+                }
+                }
